97e80613df30b6fb37c05c7d334af8c8f8a01d8d
Matteo Ponchietti <matteoponchietti@gmail.com>
2020-11-04 15:04:13 +0000

    Html e css

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+  var menuEl = $('bar-right > div')
+  var contiEl = $('#conti')
+  var productEl = $('#product')
+  var whoEl = $('#who')
+  var help = $('#help')
+  var nation = $('#nation')
+
+  var priceMenuEl = $('.prices-menu')
+  var contiMenuEl = $('.conti-menu')
+  var productMenuEl = $('.product-menu')
+  var whoMenuEl = $('.who-menu')
+  var helpMenuEl = $('.help-menu')
+  var nationMenuEl = $('.nation-menu')
+
+});

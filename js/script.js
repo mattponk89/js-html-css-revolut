892b26ea6d8c89fe5ff0b6fd9f582d6b35d8990f
Matteo Ponchietti <matteoponchietti@gmail.com>
2020-11-05 14:58:17 +0000

    DROPDOWN JS FIX

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-// $(document).ready(function () {
-//
-//   // $('.box').click(function(){
-//   //   $('.box').children().removeClass('active');
-//   //   $(this).children().addClass('active');
-//   // })
-//
-//   $(".box").hover(function () {
-//     $('.box').children().removeClass('active')
-//     $(this).children().addClass('active')
-//     }, function () {
-//     $(this).children().removeClass('active')
-//     }
-//   );
-//
-//
-// });
+$(document).ready(function () {
+
+  // $('.box').click(function(){
+  //   $('.box').children().removeClass('active');
+  //   $(this).children().addClass('active');
+  // })
+
+  $(".box").hover(function () {
+    $('.box').children('.dropMenu').removeClass('active')
+    $(this).children('.dropMenu').addClass('active')
+    }, function () {
+    $(this).children('.dropMenu').removeClass('active')
+    }
+  );
+
+
+});
